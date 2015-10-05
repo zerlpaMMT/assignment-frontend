2e1a557d4b1040f47506f5a5485950d29438ad39
Hannes Moser <hannes@impossiblearts.com>
2015-10-05 13:19:30 +0200

    refactor: autoformat

--- 02-modules/src/util.js
+++ 02-modules/src/util.js
@@ -1,27 +1,28 @@
 export function uuid() {
-  var i, random
-  var uuid = ''
+  /*jshint bitwise:false */
+  var i, random;
+  var uuid = '';
 
   for (i = 0; i < 32; i++) {
-    random = Math.random() * 16 | 0
+    random = Math.random() * 16 | 0;
     if (i === 8 || i === 12 || i === 16 || i === 20) {
-      uuid += '-'
+      uuid += '-';
     }
-    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16)
+    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
   }
 
-  return uuid
+  return uuid;
 }
 
 export function pluralize(count, word) {
-  return count === 1 ? word : word + 's'
+  return count === 1 ? word : word + 's';
 }
 
 export function store(namespace, data) {
   if (arguments.length > 1) {
-    return localStorage.setItem(namespace, JSON.stringify(data))
+    return localStorage.setItem(namespace, JSON.stringify(data));
   } else {
-    var store = localStorage.getItem(namespace)
-    return (store && JSON.parse(store)) || []
+    var store = localStorage.getItem(namespace);
+    return (store && JSON.parse(store)) || [];
   }
 }
