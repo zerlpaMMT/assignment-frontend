989571a8d16d33f439c1e7e1283538c1a2d9e587
Hannes Moser <hannes@impossiblearts.com>
2015-10-06 13:57:56 +0200

    feat: arrows ES5 and ES6

--- 03-yield/src/features/arrows.js
+++ 03-yield/src/features/arrows.js
@@ -1,7 +1,32 @@
 export function es5() {
+  var o = {
+    list: [1, 2, 3, 4, 5],
+    result: 0,
+    sum: function() {
+      var that = this
+      this.list.forEach(function(val) {
+        that.result += val
+      })
+    }
+  }
 
+  o.sum()
+
+  return o
 }
 
 export function es6() {
+  const o = {
+    list: [1, 2, 3, 4, 5],
+    result: 0,
+    sum() {
+      this.list.forEach(val => {
+        this.result += val
+      })
+    }
+  }
 
+  o.sum()
+
+  return o
 }
--- 03-yield/test/features/arrows.js
+++ 03-yield/test/features/arrows.js
@@ -1,9 +1,14 @@
+import {
+  es5,
+  es6
+} from '../../src/features/arrows'
+
 export default function() {
   it('should show arrows behavior in ES5', () => {
-
+    es5().result.should.eql(15)
   })
 
   it('should show arrows behavior in ES6', () => {
-
+    es6().result.should.eql(15)
   })
 }
