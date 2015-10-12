e36605d25a5ff08fbf92c59c116037cb2337f824
Hannes Moser <hannes@impossiblearts.com>
2015-10-12 15:47:05 +0200

    feat: add generators feature

--- 03-yield/test/features/generators.js
+++ 03-yield/test/features/generators.js
@@ -1,12 +1,23 @@
+import should from 'should'
 import {
   es5,
   es6
 } from '../../src/features/generators'
 
 export default function() {
-  it.skip('should show prototype syntax in ES5', () => {
+  it('should return an iterator in ES5', () => {
+    var next = es5(3)
+    next().should.eql(1)
+    next().should.eql(2)
+    next().should.eql(3)
+    should(next()).eql(undefined)
   })
 
-  it.skip('should show class syntax in ES6', () => {
+  it.skip('should return an iterator in ES6', () => {
+    var itr = es6(3)
+    itr.next().value.should.eql(1)
+    itr.next().value.should.eql(2)
+    itr.next().value.should.eql(3)
+    should(itr.next().done).eql(true)
   })
 }
