e3bd5e74c3f8cd509c0e0fe092a7533a2a2adccb
Hannes Moser <hannes@impossiblearts.com>
2015-10-12 10:17:42 +0200

    feat: add list of ES features

---
+++ 03-yield/test/features/subclassable-builtins.js
@@ -0,0 +1,12 @@
+import {
+  es5,
+  es6
+} from '../../src/features/classes'
+
+export default function() {
+  it.skip('should show prototype syntax in ES5', () => {
+  })
+
+  it.skip('should show class syntax in ES6', () => {
+  })
+}
