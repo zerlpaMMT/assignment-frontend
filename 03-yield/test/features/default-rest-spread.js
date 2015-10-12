c52ec373796baf997cf30b6994b6eefb4c7c7125
Hannes Moser <hannes@impossiblearts.com>
2015-10-12 11:26:13 +0200

    feat: add default-rest-spread feature

--- 03-yield/test/features/default-rest-spread.js
+++ 03-yield/test/features/default-rest-spread.js
@@ -1,10 +1,18 @@
 import {
   es5,
   es6
-} from '../../src/features/classes'
+} from '../../src/features/default-rest-spread'
 
 export default function() {
-  it.skip('should show prototype syntax in ES5', () => {
+  it('should show prototype syntax in ES5', () => {
+    var o = es5()
+    o.defaults().should.eql(12)
+    o.defaults(5).should.eql(5)
+
+    o.rest('x', 'y', 'z').should.eql('x,y,z')
+
+    o.spread([1, 2, 3]).should.eql(6)
+    o.spread([1, 2]).should.eql(3)
   })
 
   it.skip('should show class syntax in ES6', () => {
