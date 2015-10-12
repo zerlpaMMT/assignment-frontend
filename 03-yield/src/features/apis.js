a0d23d52a5e361cc058774a808990653e3c86a43
Hannes Moser <hannes@impossiblearts.com>
2015-10-12 11:02:43 +0200

    feat: add missing api feature and removed unused

--- 03-yield/src/features/apis.js
+++ 03-yield/src/features/apis.js
@@ -1,4 +1,13 @@
 export function es5() {
+  function includes(arr, val) {
+    if (arr.indexOf(val) !== -1) {
+      return true
+    }
+    return false
+  }
+
+  var a = [1, 2, 3, 4, 5]
+  return includes(a, 3)
 }
 
 export function es6() {
