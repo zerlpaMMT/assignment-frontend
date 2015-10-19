bf61ba582ecd56eca6b0d770b6c7ca926aeec823
Hannes Moser <hannes@impossiblearts.com>
2015-10-19 11:16:55 +0200

    feat: iterators + for..of

--- 03-yield/src/features/iterators-forof.js
+++ 03-yield/src/features/iterators-forof.js
@@ -1,4 +1,12 @@
 export function es5() {
+  var a = [1, 2, 3, 4, 5]
+  var sum = 0
+
+  for (var i = 0; i < a.length; i += 1) {
+    sum += a[i]
+  }
+
+  return sum
 }
 
 export function es6() {
--- 03-yield/test/features/iterators-forof.js
+++ 03-yield/test/features/iterators-forof.js
@@ -4,9 +4,11 @@ import {
 } from '../../src/features/iterators-forof'
 
 export default function() {
-  it.skip('should show prototype syntax in ES5', () => {
+  it('should return result of fibonacci iterator in ES5', () => {
+    es5().should.eql(15)
   })
 
   it.skip('should show class syntax in ES6', () => {
+    es6().should.eql(15)
   })
 }
