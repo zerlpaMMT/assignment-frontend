5fab8a4f271f9f854287679233124ee6a764f075
Hannes Moser <hannes@impossiblearts.com>
2015-10-19 11:24:04 +0200

    test: fix test descriptions

--- 03-yield/test/features/iterators-forof.js
+++ 03-yield/test/features/iterators-forof.js
@@ -4,11 +4,11 @@ import {
 } from '../../src/features/iterators-forof'
 
 export default function() {
-  it('should return result of fibonacci iterator in ES5', () => {
+  it('should return result sum of array values in ES5', () => {
     es5().should.eql(15)
   })
 
-  it.skip('should show class syntax in ES6', () => {
+  it.skip('should return sum of array values in ES6', () => {
     es6().should.eql(15)
   })
 }
