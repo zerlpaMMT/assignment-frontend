5aff45fdd9575e8fbc5a82ea430fc122b5251eef
Paul Zerlauth <pzerlauth.mmt-b2015@fh-salzburg.ac.at>
2016-11-07 00:17:53 +0100

    created 4 functions

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,18 @@
 export function valid(email) {
-
+	return (email.match(/[0-9]@fh-salzburg.ac.at/i) != null ? true : false);
 }
 
 export function degreeProgram(email) {
-
+	return email.split(".")[1].split("-")[0].toUpperCase();
 }
 
 export function level(email) {
-
+	return (email.split(".")[1].split("-")[1].toUpperCase().substring(0,1) == "B" ? "BA" : "MA");
 }
 
 export function graduationYear(email) {
+	var degreeLevel = level(email);
+	var joint = email.split(".")[1].split("-")[1].toUpperCase().substring(1,5);
 
+	return (degreeLevel == "BA" ? Number(joint)+3 : joint+2);
 }
--- 01-modules/test/index.js
+++ 01-modules/test/index.js
@@ -8,28 +8,28 @@ import {
 
 describe('01-modules', () => {
   describe('#valid()', () => {
-    it.skip('should be a valid FH/MMT email', () => {
+    it('should be a valid FH/MMT email', () => {
       valid('hmoser.mmt-b2015@fh-salzburg.ac.at').should.be.true()
     })
-    it.skip('should be an invalid FH/MMT email', () => {
+    it('should be an invalid FH/MMT email', () => {
       valid('hannes.moser@fh-salzburg.ac.at').should.be.false()
     })
   })
 
   describe('#degreeProgram()', () => {
-    it.skip('should match MMT', () => {
+    it('should match MMT', () => {
       degreeProgram('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('MMT')
     })
   })
 
   describe('#level()', () => {
-    it.skip('should match BA or MA', () => {
+    it('should match BA or MA', () => {
       level('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('BA')
     })
   })
 
   describe('#graduationYear()', () => {
-    it.skip('should be your graduation year', () => {
+    it('should be your graduation year', () => {
       graduationYear('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql(2018)
     })
   })
