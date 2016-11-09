d4ded9fa702f77c05b9232b59da0e95a6dbb769c
Paul Zerlauth <pzerlauth.mmt-b2015@fh-salzburg.ac.at>
2016-11-09 12:18:32 +0100

    refactored

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,18 +1,23 @@
 export function valid(email) {
-	return (email.match(/[0-9]@fh-salzburg.ac.at/i) != null ? true : false);
+	return (/[A-z].[A-z]{3,4}-[b|m]{1}[0-9]{4}@fh-salzburg.ac.at/i).test(email);
 }
 
 export function degreeProgram(email) {
-	return email.split(".")[1].split("-")[0].toUpperCase();
+	return (valid(email) ? email.split(".")[1].split("-")[0].toUpperCase() : null);
 }
 
 export function level(email) {
-	return (email.split(".")[1].split("-")[1].toUpperCase().substring(0,1) == "B" ? "BA" : "MA");
+	return (valid(email) ? (email.split(".")[1].split("-")[1].toUpperCase().substring(0,1) == "B" ? "BA" : "MA") : null);
 }
 
 export function graduationYear(email) {
 	var degreeLevel = level(email);
-	var joint = email.split(".")[1].split("-")[1].toUpperCase().substring(1,5);
 
-	return (degreeLevel == "BA" ? Number(joint)+3 : joint+2);
+	if(degreeLevel != null){
+		var joint = email.split(".")[1].split("-")[1].toUpperCase().substring(1,5);
+
+		return (degreeLevel == "BA" ? Number(joint)+3 : joint+2);
+	}
+	
+	return null;
 }
