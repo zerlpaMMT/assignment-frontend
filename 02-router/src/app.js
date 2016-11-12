377cdc70b7270dd600ae8e366259c3b24a3c7071
Paul Zerlauth <pzerlauth.mmt-b2015@fh-salzburg.ac.at>
2016-11-13 00:33:49 +0100

    final commit

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -1,71 +1,62 @@
-import $ from 'jquery'
-import router from './router'
-import homeTpl from './templates/home.hbs'
-import contactTpl from './templates/contact.hbs'
-import notFoundTpl from './templates/not-found.hbs'
-import magnusTpl from './templates/magnus.hbs'
-import sergeyTpl from './templates/sergey.hbs'
+import $ from 'jquery';
+import {router, goto} from './router';
+import homeTpl from './templates/home.hbs';
+import contactTpl from './templates/contact.hbs';
+import notFoundTpl from './templates/not-found.hbs';
+import magnusTpl from './templates/magnus.hbs';
+import sergeyTpl from './templates/sergey.hbs';
 
-const $app = $('#app')
 
-let definedRoutes = [
-  {"path": "/", "callback": index},
-  {"path": "/contact", "callback": contact},
-  {"path": "/players/magnus", "callback": magnus},
-  {"path": "/players/sergey", "callback": sergey},
-  {"path": "error404", "callback": notFound},
-]
-
-$(document).ready(prepareForRouting(window.location.pathname))
 
 $('a').on('click', function(){
-  let url = $(this).attr('href')
+  let url = $(this).attr('href');
 
-  if(url.indexOf('http') == -1){
-    prepareForRouting(url)
-    return false
+  if($(this).attr('rel') != "download" && $(this).attr('rel') != "external"){
+    goto(url);
+    return false;
   }
+
 })
 
+const $app = $('#app');
+
 function index() {
-  $app.html(homeTpl())
+  $app.html(homeTpl());
 }
 
 function contact() {
-  $app.html(contactTpl())
+  $app.html(contactTpl());
 }
 
-function magnus(){
-	$app.html(magnusTpl())
-}
-
-function sergey(){
-	$app.html(sergeyTpl())
+function player(partString){
+  switch(partString){
+    case "magnus":
+      $app.html(magnusTpl());
+      break;
+    case "sergey":
+      $app.html(sergeyTpl());
+      break;
+  }
 }
 
 function notFound() {
-  $app.html(notFoundTpl())
+  $app.html(notFoundTpl());
 }
 
 
-function prepareForRouting(url){
+router('/contact', contact);
+router('/players/:player', player);
+router('/players/:player/test', player);
+router('/', index);
+router('*', notFound);
 
-  let defaultMethode = definedRoutes.find(function(element){
-    return element.path == "error404"
-  })
 
-  if(url == null || url == undefined){
-    router(defaultMethode)
+$(document).ready(goto(window.location.pathname))
+
+$(window).bind('popstate', function(e){
+  e.preventDefault();
+  if(e.state !== null){
+    goto(e.originalEvent.state)
   }
 
-  let foundetMethode = definedRoutes.find(function(element){
-    return element.path == url
-  })
-
-  if(foundetMethode != undefined){
-    router(foundetMethode)
-  } else {
-    router(defaultMethode)
-  }
-
-}
+})
--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,4 +1,36 @@
-export default function({path:path="/", callback:callback}) {
-  callback()
-  history.pushState(path, "", path);
+let arr = [];
+
+export function router(path, callback) {
+  path = (path === "/" ? /^\/$/m : path);
+  var o = {regex: ((/:[a-z]*/g).test(path) ? makeRegex(path) : (path === "*" ? /\/\w+/ : new RegExp(path))), callback: callback};
+  arr.push(o);
+}
+
+export function goto(url){
+  for(var i=0; i < arr.length; i++){
+    if(arr[i].regex.test(url)){
+      var callback = arr[i].callback;
+      break;
+    }
+  }
+
+  if(url !== null && url.indexOf('players') !== -1){
+    var surl = url.split("/");
+    callback(surl[surl.length-1]);
+  } else {
+    callback();
+  }
+
+  if(window.history.state !== url){
+    window.history.pushState(url, "", url);
+  }
+
+  return false;
+}
+
+function makeRegex(url){
+  var arr = url.match(/:[a-z]*/g);
+  var newURL = url.replace(arr[0], "[a-z](.*)");
+
+  return new RegExp(newURL);
 }
