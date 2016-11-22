ee7c59170a68e8f057126ab6f0794e20ffe5463b
Paul Zerlauth <pzerlauth.mmt-b2015@fh-salzburg.ac.at>
2016-11-22 15:25:48 +0100

    solve merge conflict

--- 03-templates/src/app.js
+++ 03-templates/src/app.js
@@ -1,28 +1,9 @@
 import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
-<<<<<<< HEAD
-import magnusTpl from './templates/magnus.hbs'
-import sergeyTpl from './templates/sergey.hbs'
 import player from './templates/player.hbs'
-=======
-import playerTpl from './templates/player.hbs'
->>>>>>> upstream/master
 import contactTpl from './templates/contact.hbs'
 import notFoundTpl from './templates/not-found.hbs'
-
-const data = {
-  'magnus': {
-    name: 'Magnus Carlsen',
-    image: 'https://cdn.worldchess.com/static/img/nyfide/carlsen_2x.png',
-    description: 'Carlsen is a former chess prodigy. He became a Grandmaster in 2004, at the age of 13 years, 148 days. This made him the third-youngest grandmaster in history.'
-  },
-  'sergey': {
-    name: 'Sergey Karjakin',
-    image: 'https://cdn.worldchess.com/static/img/nyfide/karjakin_2x.png',
-    description: 'On March 28, 2016, Sergey Karjakin became the Challenger to Magnus Carlsen in the World Chess Championship 2016 after winning the Candidates Tournament 2016 in Moscow.'
-  }
-}
 
 const $app = $('#app')
 
@@ -35,8 +16,6 @@ function contact() {
 }
 
 function players(ctx) {
-<<<<<<< HEAD
-
   const players = {
     'sergey': {
       name: "Sergey",
@@ -50,20 +29,6 @@ function players(ctx) {
   }
 
   $app.html(player(players[ctx.params.player]))
-=======
-  let player = {
-    name: 'Anonymous',
-    'image': 'http://placehold.it/350x350',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }
-  switch (ctx.params.player) {
-    case 'magnus':
-    case 'sergey':
-      player = data[ctx.params.player]
-      break;
-  }
-  $app.html(playerTpl(player))
->>>>>>> upstream/master
 }
 
 function notFound() {
